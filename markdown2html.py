7b09bf04e2e431776563c70eefe0833f785e683a
8reasonswhy <yass.candidature@gmail.com>
2025-01-03 01:43:57 +0100

    0 Start a Script

--- markdown2html.py
+++ markdown2html.py
@@ -7,7 +7,7 @@ import sys
 import os
 def main():
 
-    if len(sys.argv) != 2 :
+    if len(sys.argv) > 2 :
         print("Usage: ./markdown2html.py README.md README.html", file=sys.stderr)
         sys.exit(1)
     markdown = sys.argv[1]
