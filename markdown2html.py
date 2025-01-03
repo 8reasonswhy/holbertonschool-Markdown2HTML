c70b28f3603c60c2456054924ac8e6db742f08c3
8reasonswhy <yass.candidature@gmail.com>
2025-01-03 01:42:39 +0100

    0 Start a Script

---
+++ markdown2html.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""
+Ce module prend en argument un fichier Markdown et génère un fichier HTML.
+Il vérifie que le nombre d'arguments est correct et que le fichier Markdown existe.
+"""
+import sys
+import os
+def main():
+
+    if len(sys.argv) != 2 :
+        print("Usage: ./markdown2html.py README.md README.html", file=sys.stderr)
+        sys.exit(1)
+    markdown = sys.argv[1]
+    output = sys.argv[2]
+    if not os.path.exists(markdown):
+        print("Missing markdown_file", file = sys.stderr)
+        sys.exit(1)
+    sys.exist(0)
+if __name__ == "__main__":
+    main()
